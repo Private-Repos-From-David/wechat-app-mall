21c794b5b92579c7c3641bfe926254133ce93940
杜宇 <duyub@chanjet.com>
2018-03-10 21:27:33 +0800

    update

--- app.js
+++ app.js
@@ -181,8 +181,8 @@ App({
   },
   globalData:{
     userInfo:null,
-    subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
-    version: "1.9.SNAPSHOT",
+    subDomain: "miner", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+    version: "1.0",
     shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
   }
   /*
